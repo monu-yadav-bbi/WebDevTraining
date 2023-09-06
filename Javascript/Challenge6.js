ba35bc85e8134819a67d8afca21af9560f5a6401
monu-yadav-bbi <monu.yadav@media4u.com>
2023-09-06 16:02:56 +0530

    Array Exercise

--- Javascript/Challenge6.js
+++ Javascript/Challenge6.js
@@ -1,14 +1,10 @@
-let bills = [125, 555, 44];
+const bills = [125, 555, 44];
 
-function calcTip(bill) {
-    if (bill >= 50 && bill <= 300) return bill * 0.15;
-    else { return bill * 0.2; }
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.15;
 }
 
-let tips = [];
-tips.push(calcTip(bills[0]));
-tips.push(calcTip(bills[1]));
-tips.push(calcTip(bills[2]));
+const tip = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 
-let totals = [tips[0] + bills[0], tips[1] + bills[1], tips[2] + bills[2]];
-console.log(`${totals}`);
+const totals = [bills[0] + tip[0], bills[1] + tip[1], bills[2] + tip[2]];
+console.log(bills, tip, totals);
