fee70fe730dae7512288d1925dc441b1ab8e2b00
monu-yadav-bbi <monu.yadav@media4u.com>
2023-09-08 18:27:54 +0530

    pig game, scope chain,

--- Javascript/Project2/script.js
+++ Javascript/Project2/script.js
@@ -18,13 +18,9 @@ const closeModal = function () {
 
 for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener("click", openModal);
-
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
-
 document.addEventListener("keydown", function (e) {
-  // console.log(e.key);
-
   if (e.key === "Escape" && !modal.classList.contains("hidden")) {
     closeModal();
   }
---
+++ Javascript/Project4/script.js
@@ -0,0 +1,14 @@
+// Behind the Screen
+// scope chain Practice
+"use strict";
+
+//function
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+  console.log(firstname);
+  return age;
+}
+
+//Global variable
+const firstname = "Monu";
+calcAge(1999);
